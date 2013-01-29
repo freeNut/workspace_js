a478c18d7b67bf9f30ac7d2a46f133377f77dcae
dust <zhouye071@gmail.com>
2013-01-29 15:38:11 +0800

    js_workspace
    nodejs

---
+++ microblog/public/javascripts/models/db.js
@@ -0,0 +1,5 @@
+var settings = require('../settings');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Server;
+
+module.exports = new Db(settings.db,new Server(settings.host,Connection.DEFAULT_PORT,{}));
